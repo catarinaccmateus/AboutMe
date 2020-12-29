71e4d00ffdeccfeb2e9584acd33b945b55e08983
nb26548 <nb26548@novabase.pt>
2020-12-29 12:52:40 +0000

    Setting up Data Binding

--- app/src/main/java/pt/cccm/aboutme/MainActivity.kt
+++ app/src/main/java/pt/cccm/aboutme/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Using Data Binding: binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
@@ -20,12 +21,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
     fun addNickname(view: View) {
-        val input = binding.nicknameInput
-        val nicknameView = binding.nicknameText
-        nicknameView.text = input.text
-        input.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameView.visibility = View.VISIBLE
+        // val input = binding.nicknameInput
+        // val nicknameView = binding.nicknameText
+        // nicknameView.text = input.text
+        // input.visibility = View.GONE
+        // view.visibility = View.GONE
+        // nicknameView.visibility = View.VISIBLE
+        //Same as above:
+        binding.apply {
+            nicknameText.text = nicknameInput.text
+            //invalidAll()  -> With dataBinding:  In order to refresh the UI with new data
+            nicknameInput.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
