6a0c306fad152a3dafb3caa116fecfc5e6e1a06a
nb26548 <nb26548@novabase.pt>
2020-12-29 12:32:26 +0000

    Adding a Button and TextEdit to insert a nickname

--- app/src/main/java/pt/cccm/aboutme/MainActivity.kt
+++ app/src/main/java/pt/cccm/aboutme/MainActivity.kt
@@ -1,11 +1,33 @@
 package pt.cccm.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import pt.cccm.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val button = binding.doneButton
+        button.setOnClickListener{
+            addNickname(it)
+        }
+    }
+    fun addNickname(view: View) {
+        val input = binding.nicknameInput
+        val nicknameView = binding.nicknameText
+        nicknameView.text = input.text
+        input.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameView.visibility = View.VISIBLE
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
